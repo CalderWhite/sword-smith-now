3837ede94cbd1c5189e4ffdb96785ab69e4293a6
Calder White <calderwhite1@gmail.com>
2016-12-10 21:36:30 -0500

    A couple bugs. Beginning actual game development.

--- runtime.py
+++ runtime.py
@@ -4,7 +4,7 @@ class gui(object):
 		self.parent = parent
 		parent.log("Initializing pygame...",user="GUI")
 		pygame.init()
-		self.screen = pygame.display.set_mode( (960,720) )
+		self.screen = pygame.display.set_mode( (960,720))
 		pygame.display.set_caption(parent.info["name"])
 		parent.log("Loading images....")
 		self.icon = pygame.image.load("images/icon.png")
@@ -19,7 +19,7 @@ class game(object):
 	def log(self,msg,level="INFO",user="GAME"):
 		if self.mode == 1:
 			# will only log stuff if in developer mode
-			self.dev.log(msg,level=level,user="GAME")
+			self.dev.log(msg,level=level,user=user)
 	def __init__(self,info,dev_window=None,mode=0):
 		self.mode = mode
 		#automatically change mode to 1 if a dev window is supplied
@@ -34,6 +34,7 @@ class game(object):
 	def game_loop(self):
 		pass
 	def run_start(self):
+		self.log("Entering Start Page",user="REALMS")
 		self.page = "startup"
 		self.gui.screen.fill( (255,255,255) )
 		clock = pygame.time.Clock()
@@ -41,13 +42,48 @@ class game(object):
 			# check events
 			for event in pygame.event.get():
 				if event.type == pygame.QUIT:
-					sys.exit(0)
-			# blit mouse
+					self.log("Quit event activated. Stopping game.")
+					self.page = None
+					pygame.quit()
+					pass
 			self.gui.screen.fill( (255,255,255) )
-			mouse_pos = pygame.mouse.get_pos()
-			x = mouse_pos[0] - (self.gui.cursor.get_height() / 2)
-			y = mouse_pos[1] - (self.gui.cursor.get_width() / 2)
+			mx,my = self.mouse.get_pos()
+			# load all the rectangles
+			# load all the text
+			# play button/text
+			x,y = self.gui.screen.get_size()
+			x = x/2 - 20
+			y=  y - 200
+			##print(mx,my)
+			if mx > 469 and mx < 543 and my > 521 and my < 541:
+				color = (0,128,0)
+				if self.mouse.get_pressed()[0]:
+					self.log("Entering Realm 1...",user="REALMS")
+					self.page = "realm_1"
+					self.run_realm_explorer()
+			else:
+				color = (0,0,0)
+			default_font = pygame.font.SysFont("monospace", 32,bold=True)
+			play_text = default_font.render("Play",0,color)
+			self.gui.screen.blit(play_text,(x,y))
+			# blit mouse last
+			x = mx - (self.gui.cursor.get_height() / 2)
+			y = my - (self.gui.cursor.get_width() / 2)
 			self.gui.screen.blit(self.gui.cursor,(x,y))
+			# refresh
+			pygame.display.update()
+	def run_realm_explorer(self):
+		self.page = "realm_1"
+		while self.page == "realm_1":
+			for event in pygame.event.get():
+				if event.type == pygame.QUIT:
+					self.log("Quit event activated. Stopping game.")
+					self.page = None
+					pygame.quit()
+			# clear
+			self.gui.screen.fill( (255,255,255) )
+
+			# finally, update
 			pygame.display.update()
 
 
