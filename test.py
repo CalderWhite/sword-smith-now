d6f802427ff181b68b2e7e3e9db40a89df6fb2ae
Calder White <calderwhite1@gmail.com>
2016-12-30 01:24:20 -0500

    Alpha 0.3 Snapshot v0.4.1
    Firstly, wrong version tag for that last commit.
    Secondly, adding a couple of binary files here.

--- test.py
+++ test.py
@@ -1,6 +1,15 @@
-import GMK, json
-x = GMK.items.util.grab_relic_names(print_status=True)
-j = json.dumps(x,indent=4)
-w = open("x.json",'w')
-w.write(j)
-w.close()
+import pygame, base64, codecs
+from PIL import Image, ImageFilter
+
+pygame.init()
+def blur_surf(surf):
+    # create the original pygame surface
+    #surf = pygame.image.fromstring(, size, mode)
+    size = surf.get_size()
+    raw = pygame.image.tostring(surf,"RGBA",False)
+    # create a PIL image and blur it
+    pil_blured = Image.frombytes("RGBA", size, raw).filter(ImageFilter.GaussianBlur(radius=6))
+
+    # convert it back to a pygame surface
+    filtered = pygame.image.fromstring(pil_blured.tobytes("raw", image_mode), size, image_mode)
+    return filtered
