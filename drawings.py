9513b7d3d81679aeb0e55377483ba3390416583a
Calder White <calderwhite1@gmail.com>
2016-12-31 16:48:58 -0500

    Removed weird file. Deprecated.

--- drawings.py
+++
@@ -1,7 +0,0 @@
-import pygame
-class start_page(object):
-	def __init__(self):
-		self.default_font = pygame.font.SysFont("monospace", 32)
-		self.play_text = self.default_font.render("Play",0,(0,0,0))
-pygame.init()
-start_page = start_page()
